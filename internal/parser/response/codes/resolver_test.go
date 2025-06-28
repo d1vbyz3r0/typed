43fcb6f9ed9e0747bd3f4ccead4ada795dcb82d3
d1vbyz3r0 <maksim.prytkov.2002@gmail.com>
2025-06-28 18:59:58 +0300

    extend status code resolver tests

--- internal/parser/response/codes/resolver_test.go
+++ internal/parser/response/codes/resolver_test.go
@@ -9,12 +9,41 @@ import (
 	"testing"
 )
 
-func TestResolver_Resolve(t *testing.T) {
+func TestResolver_ResolveWithConst(t *testing.T) {
 	src := `
 package test
 
 func Handler(c echo.Context) error {
 	return c.Blob(http.StatusOK, echo.MIMETextPlain, nil)	
+}
+`
+
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "", src, parser.AllErrors)
+	require.NoError(t, err)
+
+	r, err := NewResolver()
+	require.NoError(t, err)
+
+	ast.Inspect(file, func(n ast.Node) bool {
+		call, ok := n.(*ast.CallExpr)
+		if !ok {
+			return true
+		}
+
+		code, err := r.Resolve(call.Args[0])
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, code)
+		return true
+	})
+}
+
+func TestResolver_ResolveWithInt(t *testing.T) {
+	src := `
+package test
+
+func Handler(c echo.Context) error {
+	return c.Blob(200, echo.MIMETextPlain, nil)	
 }
 `
 

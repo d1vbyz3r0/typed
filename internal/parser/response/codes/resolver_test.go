137989e1ad3a5358d8e340259d5048aaa098da24
d1vbyz3r0 <maksim.prytkov.2002@gmail.com>
2025-06-28 18:30:29 +0300

    refactor status code resolver

---
+++ internal/parser/response/codes/resolver_test.go
@@ -0,0 +1,39 @@
+package codes
+
+import (
+	"github.com/stretchr/testify/require"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"net/http"
+	"testing"
+)
+
+func TestResolver_Resolve(t *testing.T) {
+	src := `
+package test
+
+func Handler(c echo.Context) error {
+	return c.Blob(http.StatusOK, echo.MIMETextPlain, nil)	
+}
+`
+
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, "", src, parser.AllErrors)
+	require.NoError(t, err)
+
+	r, err := NewResolver()
+	require.NoError(t, err)
+
+	ast.Inspect(file, func(n ast.Node) bool {
+		call, ok := n.(*ast.CallExpr)
+		if !ok {
+			return true
+		}
+
+		code, err := r.Resolve(call.Args[0])
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, code)
+		return true
+	})
+}

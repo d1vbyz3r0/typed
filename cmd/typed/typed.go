8f9ea3e8eff7589b4f3860d910e1145291799dfa
d1vbyz3r0 <maksim.prytkov.2002@gmail.com>
2025-08-23 14:52:18 +0300

    feature: add version cmd flag

--- cmd/typed/typed.go
+++ cmd/typed/typed.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/d1vbyz3r0/typed/internal/generator"
 	"log"
 	"log/slog"
 	"os"
+	"runtime/debug"
 )
 
 var (
 	configPath = flag.String("config", "", "path to config file")
+	version    = flag.Bool("version", false, "print version and exit")
 )
+
+func GetVersion() (version string) {
+	if b, ok := debug.ReadBuildInfo(); ok && len(b.Main.Version) > 0 {
+		version = b.Main.Version
+	} else {
+		version = "development"
+	}
+	return
+}
 
 func main() {
 	flag.Parse()
+
+	if *version {
+		fmt.Println(GetVersion())
+		return
+	}
 
 	if *configPath == "" {
 		log.Fatal("config path not provided")
